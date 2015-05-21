6be0538e6f8156e3aeee654e124f3ca887d5e8c4
kady <elkadyabdelrahman@gmail.com>
2015-05-21 23:46:00 +0200

    Fixing z-index problem with navbar and material buttons

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,19 @@
-/*
- * Smooth Scroll
- */
+  /*
+   * Smooth Scroll
+   */
+  $(document).ready(function() {
+    $('a').smoothScroll();
+  });
 
-$(document).ready(function(){
-  $('a').smoothScroll();
-});
+  /*
+   * Adjusting main section margin to the nav height
+   */
+  $(document).ready(function() {
+    var adjustMe = function() {
+      var navHeight = $('.navbar-material-blue.navbar').height();
+      $('.home-main').css('margin-top', -1 * navHeight + 'px');
+    };
+
+    $(window).resize(adjustMe())
+      .ready(adjustMe());
+  });
