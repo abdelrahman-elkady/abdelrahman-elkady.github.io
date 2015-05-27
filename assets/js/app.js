6da08c069dcea6245c61d7b650aaaf8b1c765683
kady <elkadyabdelrahman@gmail.com>
2015-05-27 12:57:31 +0200

    Initializing about page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,14 @@
-  /*
-   * Smooth Scroll
-   */
   $(document).ready(function() {
+    /*
+     * Smooth Scroll
+     */
     $('a').smoothScroll({
       speed: 800
     });
-  });
 
-  /*
-   * Adjusting main section margin to the nav height
-   */
-  $(document).ready(function() {
+    /*
+     * Adjusting main section margin to the nav height
+     */
     var adjustMe = function() {
       var navHeight = $('.navbar-material-blue.navbar').height();
       $('.home-header').css('margin-top', -1 * navHeight + 'px');
@@ -18,12 +16,10 @@
 
     $(window).resize(adjustMe())
       .ready(adjustMe());
-  });
 
-  /*
-   * Adding copyright with current year
-   */
-   $(document).ready(function() {
-     var copyrightText = document.createTextNode('\u00A9 abdelrahman elkady ' + (new Date()).getFullYear());
+    /*
+     * Adding copyright with current year
+     */
+    var copyrightText = document.createTextNode('\u00A9 abdelrahman elkady ' + (new Date()).getFullYear());
     $('#copyright').append($('<p>').append(copyrightText));
-   });
+  });
